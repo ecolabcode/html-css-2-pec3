6d0093c70b8683b26adf70748244b072cdd74197
Alejandro Mezza Sainz <amezza@uoc.edu>
2025-05-28 02:31:36 +0200

    tailwind in html but scss not reading

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,25 @@
 module.exports = {
   content: [
-    './src/**/*.{html,js,scss}',
+    './src/**/*.{html,js}',
   ],
   theme: {
     extend: {
       colors: {
         uocCorporate: '#eb34b7',
         uocMasterbrand: '#c0eb34',
-        uocBg: '#f0f4c3', // color.scale($uoc-masterbrand, $lightness: 70%)
+        uocBg: '#f0f9d6', // Adjusted lightness
       },
       fontFamily: {
         sans: ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'],
         serif: ['Georgia', 'Times New Roman', 'serif'],
+      },
+      fontSize: {
+        'responsive-small': '4vw',
+        'responsive-medium': '6vw',
+        'responsive-large': '8vw',
+        'small': '1rem',
+        'medium': '1.5rem',
+        'large': '2rem',
       },
     },
   },
